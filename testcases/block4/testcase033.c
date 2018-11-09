b46b316ba037c054c8f3953da77c89e39a136bb1
Ryan <ryancmeinke@gmail.com>
2018-11-09 17:45:11 -0500

    Add files via upload

---
+++ testcases/block4/testcase033.c
@@ -0,0 +1,47 @@
+#include <stdint.h>
+
+void testcase32(void)
+{
+	int x, y, z;
+	x = 0x66
+	y = 0x78;
+	z = 0;
+	if (x == 0x66)
+	{
+		if ((x - y) == 0)
+		{
+			x = add(y, 0x93);
+		}
+		else
+		{
+			x = add(y, 0x15);
+		}
+	}
+	else if (x == y)
+	{
+		if ((x - y) != 0)
+		{
+			x = add(y, 0x57);
+		}
+		else
+		{
+			x = add(y, 0x28);
+		}
+	}
+	else
+	{
+		if ((z - y) != 0)
+		{
+			x = add(y, 0x35);
+		}
+		else
+		{
+			x = add(y, 0x79);
+		}
+	}
+}
+
+uint8_t add(uint8_t x, uint8_t y)
+{
+	return x + y;
+}
