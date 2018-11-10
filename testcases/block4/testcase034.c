dc2b75e95d7d195da6f3bdb91daa2eec6df3d73d
Ryan <ryancmeinke@gmail.com>
2018-11-10 01:22:45 -0500

    Add files via upload

---
+++ testcases/block4/testcase034.c
@@ -0,0 +1,25 @@
+#include <stdint.h>
+
+void testcase32(void)
+{
+	uint8_t x, y, z;
+	y = 0x25;
+	z = 0x3;
+	for (x = 0; x < 0xa; x++)
+	{
+		if (i == 0x7)
+		{
+			x = x + 0x1;
+		}
+		else if(x == y)
+		{
+			x = 0x6;
+		}
+		else if(x == z)
+		{
+			x = 0x23;
+		}
+	}
+
+	return;
+}
